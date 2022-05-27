52d93ca5c266607ba96cc9e92c86333a8b9147ba
46_The_Doctor <33542431+Phoneix47@users.noreply.github.com>
2022-05-27 07:28:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-const x = document.getElementById('demo');
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = 'Geolocation is not supported by this browser.';
-  }
+document.addEventListener('deviceready', onDeviceReady, false);
+function onDeviceReady() {
+  console.log('navigator.geolocation works well');
 }
 
-function showPosition(position) {
-  x.innerHTML =
+// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+var onSuccess = function (position) {
+  alert(
     'Latitude: ' +
-    position.coords.latitude +
-    '<br>Longitude: ' +
-    position.coords.longitude;
+      position.coords.latitude +
+      '\n' +
+      'Longitude: ' +
+      position.coords.longitude +
+      '\n' +
+      'Altitude: ' +
+      position.coords.altitude +
+      '\n' +
+      'Accuracy: ' +
+      position.coords.accuracy +
+      '\n' +
+      'Altitude Accuracy: ' +
+      position.coords.altitudeAccuracy +
+      '\n' +
+      'Heading: ' +
+      position.coords.heading +
+      '\n' +
+      'Speed: ' +
+      position.coords.speed +
+      '\n' +
+      'Timestamp: ' +
+      position.timestamp +
+      '\n'
+  );
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+  alert(
+    'code: ' + error.code + '\n' + 'message: ' + error.message + '\n'
+  );
 }
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError);
